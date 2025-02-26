33b0b90fbf46ada4cdbffe602317354066215ca1
sallysvsta <sallyysavista@gmail.com>
2025-02-26 14:14:12 +0700

    Jobsheet3PRAKALSD

--- Mahasiswa19.java
+++ Mahasiswa19.java
@@ -1,45 +1,15 @@
-package Praktikum02;
+package Praktikum03;
 public class Mahasiswa19 {
-        String nama;
-        String nim;
-        String kelas;
-        double ipk;
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
 
-        public Mahasiswa19() {
-            
-        }
-
-        public Mahasiswa19(String nm, String nim, double ipk, String kls) {
-            nama = nm;
-            this.nim = nim;
-            this.ipk = ipk;
-            kelas = kls;
-        }
-    
-    void tampilkanInformasi() {
-        System.out.println("Nama: " + nama);
-        System.out.println("NIM: " + nim);
-        System.out.println("IPK: " + ipk);
-        System.out.println("Kelas: " + kelas);
+    void cetakInfo() {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK : " + ipk);
+        System.out.println("---------------------------");
     }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        ipk = ipkBaru;
-    }
-
-    String nilaiKinerja() {
-        if ((ipk >= 3.5)) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
-        }
-        }
-    }
+}
--- MataKuliah19.java
+++ MataKuliah19.java
@@ -1,43 +1,25 @@
-package Praktikum02;
+package Praktikum03;
 public class MataKuliah19 {
-    String kodeMK;
-    String nama;
-    int sks;
-    int jumlahJam;
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
 
     public MataKuliah19() {
-    }
 
-    public MataKuliah19(String kodeMK, String nama, int sks, int jumlahJam) {
-        this.kodeMK = kodeMK;
+    }
+    public MataKuliah19(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
 
-    void tampilInformasi() {
-        System.out.println("Kode MK: " + kodeMK);
-        System.out.println("Nama: " + nama);
+    void cetakInfo() {
+        System.out.println("Kode Mata Kuliah: " + kode);
+        System.out.println("Nama Mata Kuliah: " + nama);
         System.out.println("SKS: " + sks);
         System.out.println("Jumlah Jam: " + jumlahJam);
     }
 
-    void ubahSKS(int sksBaru) {
-        this.sks = sksBaru;
-        System.out.println("SKS telah diubah menjadi: " + this.sks);
-    }
-
-    void tambahJam(int jam) {
-        this.jumlahJam += jam;
-        System.out.println("Jumlah jam telah ditambahkan menjadi: " + this.jumlahJam);
-    }
-
-    void kurangiJam(int jam) {
-        if (this.jumlahJam >= jam) {
-            this.jumlahJam -= jam;
-            System.out.println("Jumlah jam telah dikurangi menjadi: " + this.jumlahJam);
-        } else {
-            System.out.println("Pengurangan tidak dapat dilakukan. Jumlah jam awal kurang dari jam yang akan dikurangi.");
-        }
-    }
 }
