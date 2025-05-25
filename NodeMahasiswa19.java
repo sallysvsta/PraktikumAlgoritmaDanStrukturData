1bfb8986085dcf2d3de974ea15f128dac08da8b1
sallysvsta <sallyysavista@gmail.com>
2025-05-25 15:42:28 +0700

    TUGASJOBSHEET11

--- NodeMahasiswa19.java
+++ NodeMahasiswa19.java
@@ -1,10 +1,11 @@
-package Praktikum11;
+package TugasJobsheet11;
 public class NodeMahasiswa19 {
     Mahasiswa19 data;
     NodeMahasiswa19 next;
 
-    public NodeMahasiswa19(Mahasiswa19 data, NodeMahasiswa19 next) {
+    public NodeMahasiswa19(Mahasiswa19 data) {
         this.data = data;
-        this.next = next;
+        this.next = null;
     }
-}
+    }
+
