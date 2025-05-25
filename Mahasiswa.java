9ac2882b56da25f4031441159aa1ef263962f137
sallysvsta <sallyysavista@gmail.com>
2025-05-25 16:03:33 +0700

    TUGASJOBSHEET11

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,18 +1,15 @@
-package TugasJobsheet10;
-public class Mahasiswa {
-    String nim;
+package TugasJobsheet11;
+public class Mahasiswa19 {
     String nama;
-    String prodi;
-    String kelas;
+    String nim;
+    String keperluan;
 
-    public Mahasiswa(String nim, String nama, String prodi, String kelas) {
-        this.nim = nim;
+    Mahasiswa19(String nama, String nim, String keperluan) {
         this.nama = nama;
-        this.prodi = prodi;
-        this.kelas = kelas;
+        this.nim = nim;
+        this.keperluan = keperluan;
     }
-    public void tampilkanData() {
-        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    public String toString() {
+        return nama + " - " + nim + " - " + keperluan;
     }
 }
-
---
+++ NodeMahasiswa.java
@@ -0,0 +1,11 @@
+package TugasJobsheet11;
+public class NodeMahasiswa19 {
+    Mahasiswa19 data;
+    NodeMahasiswa19 next;
+
+    public NodeMahasiswa19(Mahasiswa19 data) {
+        this.data = data;
+        this.next = null;
+    }
+    }
+
